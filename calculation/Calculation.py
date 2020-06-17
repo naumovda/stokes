4c971bfb9d7d6e0595838d9cc8a45afa3274c001
Naumov Dmitry <naumov_rgrtu@mail.ru>
2020-06-18 01:35:16 +0300

    Debugged task12.py

--- calculation/Calculation.py
+++ calculation/Calculation.py
@@ -36,57 +36,25 @@ class TIntensity:
 
 class TStokesVector:
     def __init__(self, j=0, q=0, u=0, v=0):
-        self._j = 0.0
-        self._q = 0.0
-        self._u = 0.0
-        self._v = 0.0
-        self.error_code = -1
-    
-    @property
-    def j(self):
-        return self._j
-
-    @j.setter
-    def set_j(self, value):
-        self._j = value
+        self.J = j
+        self.Q = q
+        self.U = u
+        self.V = v
+        self.error_code = -1  
 
     @property
-    def q(self):
-        return self._q
-
-    @q.setter
-    def set_q(self, value):
-        self._q = value
-
-    @property
-    def u(self):
-        return self._u
-
-    @u.setter
-    def set_u(self, value):
-        self._u = value
-
-    @property
-    def v(self):
-        return self._v
-
-    @v.setter
-    def set_v(self, value):
-        self._v = value
-
-    @property
-    def p(self):
-        if abs(self.j) < 1e-8:
+    def P(self):
+        if abs(self.J) < 1e-8:
             return 0.0
         else:
-            result = sqrt(self.q**2 + self.u**2 + self.v**2) / self.j
+            result = sqrt(self.Q**2 + self.U**2 + self.V**2) / self.J
             if result > 1:
                 result = 1
             return result
     
     def calculate(self, I1, I2, I3, I4, WriteLog):
-        A = [([None] * 5) for _ in range(4)]
-        B = X = [None] * 4
+        A = [([0] * 5) for _ in range(4)]
+        X = [0] * 4
 
         def Swap(k, n):
             z = A[k][k]
@@ -98,7 +66,6 @@ class TStokesVector:
             if i > k:
                 for j in range(k, n):
                     A[i][j], A[k][j] = A[k][j], A[i][j]
-                B[i], B[k] = B[k], B[i]
         
         def LogMatrix():
             if not WriteLog:
@@ -106,38 +73,33 @@ class TStokesVector:
             LogFile.write('------------------------------------------------------\n')
             for i in range(4):
                 for j in range(4):
-                    LogFile.write(f'{round(A[i][j], 2)}'.ljust(12))
-                LogFile.write(f' | {B[i]}\n')
+                    LogFile.write(f'{A[i][j]:9.4f}')
+                LogFile.write(f' | {A[i][4]:9.4f}\n')
         
         self.ErrorCode = -1
-        self.j = 0
-        self.q = 0
-        self.u = 0
-        self.v = 0
+        self.J = 0
+        self.Q = 0
+        self.U = 0
+        self.V = 0
 
         if WriteLog:
-            LogFile = open('LogPart01.txt', 'w')
+            LogFile = open('LogPart01.txt', 'w', encoding="utf-8")
             LogFile.writelines(
                 ('Расчет параметров вектора Стокса рассеянного излучения\n',
                 '------------------------------------------------------\n',
-                f'I1 = {I1.i}\n',
-                f'    tau1 = {I1.tau}\n',
-                f'    phi1 = {I1.phi}\n',
-                f'I2 = {I2.i}\n',
-                f'    tau2 = {I2.tau}\n',
-                f'    phi2 = {I2.phi}\n',
-                f'I3 = {I3.GetI()}\n',
-                f'    tau3 = {I3.tau}\n',
-                f'    phi3 = {I3.phi}\n',
-                f'I4 = {I4.i}\n',
-                f'    tau4 = {I4.tau}\n',
-                f'    phi4 = {I4.phi}\n')
+                f'I1 = {I1.i:9.4f}\n',
+                f'    tau1 = {I1.tau:9.4f}\n',
+                f'    phi1 = {I1.phi:9.4f}\n',
+                f'I2 = {I2.i:9.4f}\n',
+                f'    tau2 = {I2.tau:9.4f}\n',
+                f'    phi2 = {I2.phi:9.4f}\n',
+                f'I3 = {I3.i:9.4f}\n',
+                f'    tau3 = {I3.tau:9.4f}\n',
+                f'    phi3 = {I3.phi:9.4f}\n',
+                f'I4 = {I4.i:9.4f}\n',
+                f'    tau4 = {I4.tau:9.4f}\n',
+                f'    phi4 = {I4.phi:9.4f}\n')
             )
-
-        B[0] = 2 * I1.i
-        B[1] = 2 * I2.i
-        B[2] = 2 * I3.i
-        B[3] = 2 * I4.i
 
         A[0][0] = 1
         A[1][0] = 1
@@ -159,13 +121,13 @@ class TStokesVector:
         A[2][3] = sin(2 * I3.phi) * sin(I3.tau)
         A[3][3] = sin(2 * I4.phi) * sin(I4.tau)
 
-        LogMatrix()
+        A[0][4] = 2 * I1.i
+        A[1][4] = 2 * I2.i
+        A[2][4] = 2 * I3.i
+        A[3][4] = 2 * I4.i
 
         n = 4
-
-        for i in range(n):  
-            A[i][n] = B[i]
-            X[i] = 0
+        LogMatrix()
 
         for k in range(n):
             if abs(A[k][k]) < 1e-10:
@@ -185,7 +147,7 @@ class TStokesVector:
                 r = A[j][k] / A[k][k]
                 for i in range(n + 1):
                     A[j][i] -= r * A[k][i]
-                B[j] -= r * B[k]
+                # B[j] -= r * B[k]
 
             LogMatrix()
     
@@ -194,30 +156,34 @@ class TStokesVector:
             for j in range(k + 1, n):
                 g = A[k][j] * X[j]
                 r += g
-            X[k] = (B[k] - r) / A[k][k]
+            if A[k][k] == 0:
+                self.error_code = -1
+                LogFile.close()
+                return
+            X[k] = (A[k][4] - r) / A[k][k]
         
-        self.j = X[0]
-        self.q = X[1]
-        self.u = X[2]
-        self.v = X[3]
+        self.J = X[0]
+        self.Q = X[1]
+        self.U = X[2]
+        self.V = X[3]
 
         if WriteLog:
             LogFile.writelines(
                 ('------------------------------------------------------\n',
-                f'J = {X[0]}\n',
-                f'Q = {X[1]}\n',
-                f'U = {X[2]}\n',
-                f'V = {X[3]}\n',
+                f'J = {X[0]:9.4f}\n',
+                f'Q = {X[1]:9.4f}\n',
+                f'U = {X[2]:9.4f}\n',
+                f'V = {X[3]:9.4f}\n',
                 '------------------------------------------------------\n',
-                f'P = {self.p}\n',
+                f'P = {self.P:9.4f}\n',
                 '------------------------------------------------------\n')
             )
             LogFile.close()
         
-        self.ErrorCode = 0
+        self.error_code = 0
 
 def SquareRoot(v, index):
-    a = sqrt(v.real**2 + v.imag**2)
+    a = sqrt(sqrt(v.real**2 + v.imag**2))
     phi = atan(v.imag/v.real)
     if v.real < 0:
         phi += pi
@@ -228,21 +194,21 @@ def SquareRoot(v, index):
     return complex(a*cos(phi), a*sin(phi))
 
 class TStokesNaturalVector(TStokesVector):
-    def __init__(self, j=0, q=0, u=0, v=0):
-        super.__init__(self, j, q, u, v)
+    def __init__(self, J=0, Q=0, U=0, V=0):
+        super().__init__(J, Q, U, V)
 
     def calculateNatural(self, vector, nju, phi, WriteLog):
         if WriteLog:            
-            LogFile = open('LogPart02.txt', 'w')            
+            LogFile = open('LogPart02.txt', 'w', encoding="utf-8")            
             LogFile.writelines(
                 ('Расчет параметров вектора Стокса естественного излучения\n',
                 '------------------------------------------------------\n',
                 'Вектор Стокса рассеянного излучения\n',
-                f'    J = {vector.j}\n',
-                f'    Q = {vector.q}\n',
-                f'    U = {vector.u}\n',
-                f'    V = {vector.v}\n',
-                f'    P = {vector.p}\n')
+                f'    J = {vector.J:9.4f}\n',
+                f'    Q = {vector.Q:9.4f}\n',
+                f'    U = {vector.U:9.4f}\n',
+                f'    V = {vector.V:9.4f}\n',
+                f'    P = {vector.P:9.4f}\n')
             )
         
         nju2 = nju**2
@@ -255,7 +221,7 @@ class TStokesNaturalVector(TStokesVector):
         sinphi2 = sinphi * sinphi
         sn = nju2 - sinphi2
 
-        sq = SquareRoot(sn, 0)        
+        sq = SquareRoot(sn, 0)      
         if sq.imag > 0:
             sq = SquareRoot(sn, 1)
 
@@ -272,38 +238,38 @@ class TStokesNaturalVector(TStokesVector):
         r1r2_ = r1 * r2.conjugate()
         r1r2_2 = abs(r1r2_) ** 2
 
-        self.j = (vector.GetJ() * (r1_2 + r2_2) - vector.GetQ() * (r1_2 - r2_2)) / (2 * r1_2 * r2_2)
-        self.q = (vector.GetQ() * (r1_2 + r2_2) - vector.GetJ * (r1_2 - r2_2)) / (2 * r1_2 * r2_2)
-        self.u = (vector.GetU() * r1r2_.Real() + vector.GetV() * r1r2_.Image()) / r1r2_2
-        self.v = (vector.GetV() * r1r2_.Real() - vector.GetU() * r1r2_.Image()) / r1r2_2
+        self.J = (vector.J * (r1_2 + r2_2) - vector.Q * (r1_2 - r2_2)) / (2 * r1_2 * r2_2)
+        self.Q = (vector.Q * (r1_2 + r2_2) - vector.J * (r1_2 - r2_2)) / (2 * r1_2 * r2_2)
+        self.U = (vector.U * r1r2_.real + vector.V * r1r2_.imag) / r1r2_2
+        self.V = (vector.V * r1r2_.real - vector.U * r1r2_.imag) / r1r2_2
         
         if WriteLog:
             LogFile.writelines(
             ('Исходные параметры\n',
             '------------------------------------------------------\n',
-            f' Phi                    = {phi} град.\n',
-            f' Nju                    = ({nju.real}, {nju.imag})\n',
+            f' Phi                    = {phi:9.4f} град.\n',
+            f' Nju                    = ({nju.real:9.4f}, {nju.imag:9.4f})\n',
             '------------------------------------------------------\n',
             'Промежуточные переменные\n',
             '------------------------------------------------------\n',
-            f' Nju^2                  = ({nju2.real}, {nju2.imag})\n',
-            f' Nju^2-sin(Phi)^2       = ({sn.real}, {sn.imag})\n',
-            f' SQRT(Nju^2-sin(Phi)^2) = ({sq.real}, {sq.imag})\n',
-            f' r1                     = ({r1.real}, {r1.imag})\n',
-            f' r2                     = ({r2.real}, {r2.imag})\n',
+            f' Nju^2                  = ({nju2.real:9.4f}, {nju2.imag:9.4f})\n',
+            f' Nju^2-sin(Phi)^2       = ({sn.real:9.4f}, {sn.imag:9.4f})\n',
+            f' SQRT(Nju^2-sin(Phi)^2) = ({sq.real:9.4f}, {sq.imag:9.4f})\n',
+            f' r1                     = ({r1.real:9.4f}, {r1.imag:9.4f})\n',
+            f' r2                     = ({r2.real:9.4f}, {r2.imag:9.4f})\n',
 
-            f' |r1|^2                 = {r1_2}\n',
-            f' |r2|^2                 = {r2_2}\n',
+            f' |r1|^2                 = {r1_2:9.4f}\n',
+            f' |r2|^2                 = {r2_2:9.4f}\n',
 
-            f' r1*_r2                 = {r1r2_.real}, {r1r2_.imag})\n',
-            f' |r1*_r2|^2             = {r1r2_2}\n',
+            f' r1*_r2                 = {r1r2_.real:9.4f}, {r1r2_.imag:9.4f})\n',
+            f' |r1*_r2|^2             = {r1r2_2:9.4f}\n',
             '------------------------------------------------------\n',
             'Вектор Стокса естественного излучения\n',
-            f'  J0                    = {self.j}\n',
-            f'  Q0                    = {self.q}\n',
-            f'  U0                    = {self.u}\n',
-            f'  V0                    = {self.v}\n',
-            f'  P0                    = {self.p}\n')
+            f'  J0                    = {self.J:9.4f}\n',
+            f'  Q0                    = {self.Q:9.4f}\n',
+            f'  U0                    = {self.U:9.4f}\n',
+            f'  V0                    = {self.V:9.4f}\n',
+            f'  P0                    = {self.P:9.4f}\n')
             )
             LogFile.close()
 
--- calculation/task12.py
+++ calculation/task12.py
@@ -1,72 +1,77 @@
-from math import sqrt, pi, sin, cos, atan
-from cmath import phase
 import os
+import csv
+
+from math import sqrt, pi, sin, cos, tan, atan
+from cmath import phase
+
 from calculation import TIntensity, TStokesVector, TStokesNaturalVector
 from gradient import Gradient
 
 class TTask12:
-    def __init__(self, WriteLog=False, isAnalitic=True):
-        self.WriteLog = WriteLog
-        self.isAnalitic = isAnalitic
+    def __init__(self, Idx, Alfa, Beta):
+        self.Idx = Idx
+        self.Alfa = Alfa
+        self.Beta = Beta
         self.V = TStokesVector()
         self.N = TStokesNaturalVector()
         self.I = [TIntensity(0, 0, 0) for i in range(4)]
 
     def load(self, index, dI, Tau, Phi):
+        dI = float(dI)
+        Tau = float(Tau)
+        Phi = float(Phi)
         self.I[index] = TIntensity(dI, Tau, Phi)
 
-    def calcRadiation(self, nju, phi):
+    def calcRadiation(self, nju, phi, WriteLog=False):
         self.V = TStokesVector()
-        self.V.calculate(self.I[0], self.I[1], self.I[2], self.I[3], self.WriteLog)
+        self.V.calculate(self.I[0], self.I[1], self.I[2], self.I[3], WriteLog)
         self.N = TStokesNaturalVector()
-        self.N.calculateNatural(self.V, nju, phi, self.WriteLog)         
+        self.N.calculateNatural(self.V, nju, phi, WriteLog)         
         return (self.V, self.N)
 
-    def calcReflection(self, nju, phi, gradient, isAnalitic=True):
-        self.V = TStokesVector()
-        self.N = TStokesNaturalVector()
+    def calcReflection(self, nju, phi, gradient, isAnalitic=True, WriteLog=False):
+        # self.V = TStokesVector()
+        # self.N = TStokesNaturalVector()
 
-        self.V, self.N = self.calcRadiation(nju, phi)
+        self.V, self.N = self.calcRadiation(nju, phi, WriteLog)
 
-        J = self.V.j
-        Q = self.V.q
-        U = self.V.u
-        V = self.V.v
-        # P = self.V.p
+        J = self.V.J
+        Q = self.V.Q
+        U = self.V.U
+        V = self.V.V
 
-        if Q ** 2 + V ** 2 + U ** 2 < 1e-5:
+        if Q**2 + V**2 + U**2 < 1e-5:
             gradient.W = 0
             gradient.V = 0
         else:
             gradient.W = Q / sqrt(Q ** 2 + V **2 + U ** 2)
             gradient.V = U / sqrt(Q ** 2 + V **2 + U ** 2)
         
-        x = x0 = 0 # в исходнике не задаётся начальное значение, в паскале в таком случае её исходное значение равно 0, здесь же будет ошибка
+        x = x0 = 0 
         y = y0 = 0
         x1 = x2 = 0
     
-        if self.WriteLog:        
-            # os.chdir(os.path.dirname(__file__)) 
-            LogFile = open('LogPart03.txt', 'w')
+        if WriteLog:        
+            LogFile = open('LogPart03.txt', 'w', encoding="utf-8")
 
             LogFile.writelines(
                 ('Расчет параметров поляризации\n',
                 '------------------------------------------------------\n',
-                f'J = {J}\n',
-                f'Q = {Q}\n',
-                f'U = {U}\n',
-                f'V = {V}\n',
+                f'J = {J:9.4f}\n',
+                f'Q = {Q:9.4f}\n',
+                f'U = {U:9.4f}\n',
+                f'V = {V:9.4f}\n',
                 '------------------------------------------------------\n',
-                f'Gamma = {gradient.GAMMA}\n',
-                f'Q / sqrt(Q*Q + V*V + U*U) = {gradient.W}\n',
-                f'U / sqrt(Q*Q + V*V + U*U) = {gradient.V}\n',
+                f'Gamma = {gradient.GAMMA:9.4f}\n',
+                f'Q / sqrt(Q*Q + V*V + U*U) = {gradient.W:9.4f}\n',
+                f'U / sqrt(Q*Q + V*V + U*U) = {gradient.V:9.4f}\n',
                 '------------------------------------------------------\n',
                 'Начальное приближение\n',
-                f'x0 = {x0}\n',
-                f'y0 = {y0}\n')
+                f'x0 = {x0:9.4f}\n',
+                f'y0 = {y0:9.4f}\n')
             )
             
-        if self.isAnalitic:
+        if isAnalitic:
             isDone = True
             Beta = abs(atan(V/sqrt(Q**2+U**2+(cos(gradient.GAMMA)**2)*(Q**2+V**2+U**2))))
 
@@ -80,20 +85,19 @@ class TTask12:
             elif U < 0:
                 Alpha *= -1
 
-            if self.WriteLog:        
+            if WriteLog:        
                 LogFile.writelines( 
                     ('------------------------------------------------------\n',
                     'Найдено аналитическим методом:\n',
-                    f'alpha = {Alpha}\n',
-                    f'beta = {Beta}\n',
+                    f'alpha = {Alpha:9.4f}\n',
+                    f'beta  = {Beta:9.4f}\n',
                     '------------------------------------------------------\n')
                     )
         else: 
             x, y, isDone = gradient.GradientMethod(x0, y0, 1e-5)
 
             if not isDone:
-                # Редактирование БД (фрагмент кода опущен)
-                if self.WriteLog:
+                if WriteLog:
                     LogFile.writelines(
                         ('------------------------------------------------------\n',
                         'Метод градиента не смог найти минимум функции\n',
@@ -103,24 +107,23 @@ class TTask12:
                 return
             
             if abs(gradient.Fxy(x, y) > 1e-5):
-                # Редактирование БД (фрагмент кода опущен)
-                if self.WriteLog:
+                if WriteLog:
                     LogFile.writelines(
                         ('------------------------------------------------------\n',
                         'Ошибка: система не имеет решения\n',
-                        f'cos(2a)cos(2b) - cos(gamma)sin(2a)sin(2b) = {gradient.W}\n',
-                        f'sin(2a)cos(2b) + cos(gamma)sin(2a)sin(2b) = {gradient.V}\n',
+                        f'cos(2a)cos(2b) - cos(gamma)sin(2a)sin(2b) = {gradient.W:9.4f}\n',
+                        f'sin(2a)cos(2b) + cos(gamma)sin(2a)sin(2b) = {gradient.V:9.4f}\n',
                         '------------------------------------------------------\n')
                     )
                     LogFile.close()
                 return
             
-            if self.WriteLog:
+            if WriteLog:
                 LogFile.writelines(
                     ('------------------------------------------------------\n',
                     'Найдено решение методом градиента\n',
-                    f'alpha1 = {x}\n',
-                    f'beta1 = {y}\n',
+                    f'alpha1 = {x:9.4f}\n',
+                    f'beta1  = {y:9.4f}\n',
                     '------------------------------------------------------\n')
                 )
             
@@ -130,11 +133,10 @@ class TTask12:
             d = b ** 2 - 4 * c * a
 
             if d < 0:
-                # Редактирование БД (фрагмент кода опущен)
-                if self.WriteLog:
+                if WriteLog:
                     LogFile.writelines(
                         ('------------------------------------------------------\n',
-                        f'Ошибка: не найдено второе решение alpha 2 для beta2 = {-y}\n',
+                        f'Ошибка: не найдено второе решение alpha 2 для beta2 = {-y:9.4f}\n',
                         '------------------------------------------------------\n')
                     )
                     LogFile.close()
@@ -150,37 +152,63 @@ class TTask12:
             else:
                 Beta = abs(y)
             
-        hi = complex(sin(Alpha)/cos(Alpha), sin(Beta)/cos(Beta))
-        down = complex(1, -sin(Alpha)*sin(Beta)/cos(Alpha)/cos(Beta))
-        hi = hi/down
+        res = complex(tan(Alpha), tan(Beta)) / complex(1, -tan(Alpha)*tan(Beta))
         
-        # Редактирование БД (фрагмент кода опущен)
-        if self.WriteLog:
+        if WriteLog:
             LogFile.writelines(
-                (f'alpha1 = {x}\n', # UnboundLocalError: local variable 'x' referenced before assignment, поэтому задаю начальное значение x = 0
-                f'beta1 = {y}\n',
-                f'alpha2 = {x2}\n',
-                f'beta2 = {-y}\n',
+                (f'alpha1 = {x:9.4f}\n', 
+                f'beta1   = {y:9.4f}\n',
+                f'alpha2  = {x2:9.4f}\n',
+                f'beta2   = {-y:9.4f}\n',
                 '------------------------------------------------------\n',
-                f'alpha = (alpha1+alpha2)/2 = {round(Alpha, 2)}\n', # нужно ли округлять?
-                f'beta = {round(Beta, 2)}\n',
+                f'alpha = (alpha1+alpha2)/2 = {Alpha:9.4f}\n', 
+                f'beta = {Beta:9.4f}\n',
                 '------------------------------------------------------\n',
-                f'Re(Hi) = {hi.real}\n',
-                f'Im(Hi) = {hi.imag}\n',
-                f'|Hi|   = {abs(hi)}\n',
-                f'/_Hi   = {phase(hi)}, рад. = {phase(hi)/pi*180} град.\n',
+                f'Re(Hi) = {res.real:9.4f}\n',
+                f'Im(Hi) = {res.imag:9.4f}\n',
+                f'|Hi|   = {abs(res):9.4f}\n',
+                f'/_Hi   = {phase(res):9.4f}, рад. = {phase(res)/pi*180:9.4f} град.\n',
                 '------------------------------------------------------\n',
                 'Расчет завершен\n',
                 '------------------------------------------------------\n')
             )
             LogFile.close()
-    
-if __name__ == '__main__':
-    # from random import randrange
-    # SourceData = TSourceData(WriteLog = True, isAnalitic = True) # для проверки всего кода следует тестировать с isAnalitic = True / False
-    # SourceData.acCaclReflectionExecute()
-    # SourceData.acCalcRadiationExecute()
-    # if SourceData.WriteLog:
-    #     os.startfile('LogPart01.txt')
-    #     os.startfile('LogPart03.txt')
-    pass
+
+def loadTask12(filename):
+    """
+    Read a CSV file using csv.DictReader
+    """
+    data = []
+    with open(filename) as f_obj:
+        reader = csv.DictReader(f_obj, delimiter=';')
+        for line in reader:
+            item = TTask12(int(line['ObjectId']), line['Alfa'], line['Beta'])
+            item.load(0, line['I1'], line['Tau1'], line['Phi1'])
+            item.load(1, line['I2'], line['Tau2'], line['Phi2'])
+            item.load(2, line['I3'], line['Tau3'], line['Phi3'])
+            item.load(3, line['I4'], line['Tau4'], line['Phi4'])            
+            data.append(item)
+    return data
+
+def calcTask1(data, nju, phi, WriteLog=False):
+    for item in data:
+        if item.Idx == 174:
+            item.calcRadiation(nju, phi, WriteLog)
+        # break
+
+def calcTask2(data, nju, phi, gradient, isAnalitic=True, WriteLog=False):
+    for item in data:
+        if item.Idx == 174:
+            item.calcReflection(nju, phi, gradient, isAnalitic, WriteLog)
+        # break
+
+if __name__ == '__main__':  
+    os.chdir(os.path.dirname(__file__))     
+    data = loadTask12('../data/csv/calculation.csv')
+
+    nju = 1.4 - 4.53j
+    phi = 52
+    g = Gradient()
+
+    calcTask1(data, nju, phi, True) 
+    calcTask2(data, nju, phi, g, WriteLog=True)
