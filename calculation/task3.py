00465eaf27436bc7ed1d2375f90e3f2aded6d408
Naumov Dmitry <lonlymage@mail.ru>
2020-06-18 16:18:42 +0300

    Finished testing task03-2

--- calculation/task3.py
+++ calculation/task3.py
@@ -258,11 +258,11 @@ def calcStat(data):
 IS_DIELECTRIC = 0
 IS_METALLIC = 1
 
-class TCalc3:
-    def __init__(self, Tetta, Lambda, N):
+class TSample:
+    def __init__(self, Tetta, Lambda):
         self.Tetta = Tetta
         self.Lambda = Lambda
-        self.N = N
+        self.N = None
 
 class TParam:
     def __init__(self, Tetta1, Tetta2, A1, A2, V1, V2, U1, U2):
@@ -285,54 +285,61 @@ class TMaterial:
         self.im_max = im_max
         self.length = 0
 
-def calcPlaneMaterial(data, materials, planeType, writeLog):
+def calcPlaneMaterial(data, materials, planeType, writeLog=True, logFileName='LogPart05.txt'):
     if writeLog:
-        LogFile = open('LogPart03.txt', 'w')
+        LogFile = open(logFileName, 'w', encoding="utf-8")
         LogFile.writelines(
-            'Определение материала покрытия\n',
-            '------------------------------------------------------\n'
+            ('Определение материала покрытия\n',
+            '------------------------------------------------------\n')            
         )
 
         if planeType == IS_DIELECTRIC:
-            LogFile.writelines('  Расчет коэффициента преломления диэлектрического покрытия')
+            LogFile.writelines('  Расчет коэффициента преломления диэлектрического покрытия\n')
         elif planeType == IS_METALLIC:
-            LogFile.writelines('  Расчет коэффициента преломления металлического покрытия')
+            LogFile.writelines('  Расчет коэффициента преломления металлического покрытия\n')
         else:
             pass
 
+    i = 0
     idx = 0
-    val = min([item.Lambda for item in data])
+    val = data[0].Lambda
     for item in data:
-        if val == item.Lambda:
-            break
-        idx += 1
+        if item.Lambda < val:            
+            idx = i
+            val = item.Lambda
+        i += 1
 
     if writeLog:
         LogFile.writelines(
-            f'  индекс точки минимума = {idx}\n',
+            (f'  индекс точки минимума = {idx}\n',
             '------------------------------------------------------\n'
-            'Исходные данные для расчета\n'
+            'Исходные данные для расчета\n')
         )
 
     if writeLog:
         if planeType == IS_DIELECTRIC:
-            LogFile.writelines('Tetta Lambda N')
+            LogFile.writelines('Tetta Lambda N\n')
         elif planeType == IS_METALLIC:
-            LogFile.writelines('Tetta Lambda A')
+            LogFile.writelines('Tetta Lambda A\n')
         else:
             pass
     
     # вычисляем значение n
     i = 0    
     for item in data:
-        Tetta = pi * item.Tetta.Value/180
+        Tetta = pi*item.Tetta/180.0
         Lambda = item.Lambda
 
         if planeType == IS_DIELECTRIC:
+
             if i <= idx:
-                item.N = tan(Tetta)*sqrt(1+Lambda**2-2*Lambda*cos(2*Tetta))/(1-Lambda)
+                item.N = sin(Tetta)/cos(Tetta)\
+                    *sqrt(1+Lambda**2-2*Lambda*cos(2*Tetta))\
+                        /(1-Lambda)
             else:
-                item.N = tan(Tetta)*sqrt(1+Lambda**2-2*Lambda*cos(2*Tetta))/(1+Lambda)
+                item.N = sin(Tetta)/cos(Tetta)\
+                    *sqrt(1+Lambda**2+2*Lambda*cos(2*Tetta))\
+                        /(1+Lambda)
         elif planeType == IS_METALLIC:
             if i <= idx:
                 item.N = sin(Tetta)**2/cos(Tetta)*(1+Lambda)/(1-Lambda)
@@ -340,17 +347,18 @@ def calcPlaneMaterial(data, materials, planeType, writeLog):
                 item.N = sin(Tetta)**2/cos(Tetta)*(1-Lambda)/(1+Lambda)
         else:
             pass
+        i += 1
 
-    nmid = mean([item.N for item in data])
+    nmid = sum([item.N for item in data])/(len(data)-1)
 
     if writeLog:
         for item in data:
-            LogFile.writelines(f'{item.Tetta*180/pi} {item.Lambda} {item.N}') 
+            LogFile.writelines(f'{item.Tetta:0.4f}\t{item.Lambda:0.4f}\t{item.N:0.4f}\n') 
 
     if planeType == IS_DIELECTRIC:
         coef = complex(nmid, 0)
     elif planeType == IS_METALLIC:
-        count = len(data)
+        count = len(data)-1
         if count // 2 == 1:
             ParamCount = count // 2 + 1
             i2 = count-1
@@ -358,13 +366,13 @@ def calcPlaneMaterial(data, materials, planeType, writeLog):
             ParamCount = count // 2
             i2 = round(count/2)
 
-    if writeLog:
-        LogFile.writelines(
-            '------------------------------------------------------\n',
-            'начальный индекс i1  = 0\n',
-            f'начальный индекс i2  = {i2}\n',
-            f'количество уравнений = {ParamCount}' 
-        )
+        if writeLog:
+            LogFile.writelines(
+                ('------------------------------------------------------\n',
+                'начальный индекс i1  = 0\n',
+                f'начальный индекс i2  = {i2}\n',
+                f'количество уравнений = {ParamCount}\n')
+            )
 
         params = []
         for _ in range(ParamCount):
@@ -380,9 +388,9 @@ def calcPlaneMaterial(data, materials, planeType, writeLog):
 
         if writeLog:
             LogFile.writelines(
-                '------------------------------------------------------\n',
+                ('------------------------------------------------------\n',
                 'Tetta1, A1, Tetta2, A2, V, U\n',
-                '------------------------------------------------------\n',                    
+                '------------------------------------------------------\n')  
             )
         
         u_mid = 0
@@ -433,25 +441,32 @@ def calcPlaneMaterial(data, materials, planeType, writeLog):
 
         if writeLog:
             LogFile.writelines(
-                '------------------------------------------------------\n',
+                ('------------------------------------------------------\n',
                 ' Vmid = {Vmid}\n',
                 ' Umid = {Umid}\n', 
                 '------------------------------------------------------\n', 
                 'Коэффициент преломления:\n',
                 'Re={coef.real}:\n',
                 'Im={coef.imag}:\n',
-                '------------------------------------------------------\n'
+                '------------------------------------------------------\n')
             )
-    else:
-        pass
 
     if writeLog:
-        LogFile.writelines(f'Коэффициент преломления: {coef}') 
+        LogFile.writelines(f'Коэффициент преломления: ({coef.real:0.4f}+{coef.imag:0.4f}j)\n') 
 
+    if writeLog:
+        LogFile.writelines(
+            ('------------------------------------------------------\n',
+            'Материалы, Коэффициент\n')
+        ) 
+
+    i = 0
     idx = 0
     length = 1e+38
     for item in materials:
-        item = TMaterial('','',0,0,0,0)
+        if writeLog:
+            LogFile.writelines(f'{item.name}, {item.label}\n') 
+
         item.length = len2rect(
             coef.real, coef.imag, 
             item.re_min, item.re_max,
@@ -459,14 +474,14 @@ def calcPlaneMaterial(data, materials, planeType, writeLog):
         )
         if item.length < length:
             idx, length = i, item.length
-        idx += 1
+        i += 1
 
     if writeLog:
         LogFile.writelines(
-            '------------------------------------------------------\n',
+            ('------------------------------------------------------\n',
             f'Наиболее близкий по характеристике материал к Re={coef.real}, Im={coef.imag}:\n',
             f'{materials[idx].name}\n',
-            f'{materials[idx].label}\n'
+            f'{materials[idx].label}\n')
         ) 
 
     LogFile.close()
@@ -543,6 +558,20 @@ def loadMaterial(filename):
             data.append(item)
     return data 
 
+def loadSamples(filename):
+    """
+    Read a samples file for plane type calculation
+    """
+    data = []
+    with open(filename, encoding="utf-8") as f_obj:
+        reader = csv.DictReader(f_obj, delimiter=';')
+        for line in reader:
+            Tetta = float(line['dTetta'])
+            Lambda = float(line['Lambda'])
+            item = TSample(Tetta, Lambda)
+            data.append(item)
+    return data 
+
 def printStat(stat, filename, mode='w', title=None):
     LogFile = open(filename, mode, encoding="utf-8")   
     if title != None:
@@ -558,7 +587,7 @@ def calcTask3(data, nju, phi, WriteLog=False, Idx=None):
             pass
             # item.calcRadiation(nju, phi, WriteLog)
 
-if __name__ == "__main__":
+def test_Task03():
     os.chdir(os.path.dirname(__file__))     
 
     c1 = loadC1('../data/csv/C1.csv')
@@ -579,4 +608,17 @@ if __name__ == "__main__":
     LogFile.writelines(f'Вероятность попадания объекта в класс = {p1*p2*p3:0.4f}')
     LogFile.close()
 
-    # materials = loadMaterial('../data/csv/MaterialRefraction.csv')
+def test_Task04():
+    os.chdir(os.path.dirname(__file__))     
+    
+    materials = loadMaterial('../data/csv/MaterialRefraction.csv')   
+    
+    data = loadSamples('../data/csv/task03_test01_diel.csv')
+    planeType = IS_DIELECTRIC
+    _, _ = calcPlaneMaterial(data, materials, planeType, \
+        writeLog=True, logFileName='LogPart05_test01.txt')    
+    # print(idx, material)
+
+if __name__ == "__main__":   
+    # test_Task03() 
+    test_Task04() 
